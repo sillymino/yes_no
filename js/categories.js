c7dd92625a6a85d4dff1a5d017151c1ee6c4cf7e
Nikki <nicole16@mit.edu>
2014-07-10 13:20:49 -0400

    formatted data for this HIT

--- js/categories.js
+++ js/categories.js
@@ -3,40 +3,48 @@ var cats = [
     "Yes",
     "No",
     "Not Sure",
-    "Irrelevant"
+    "Irrelevant",
+    "NA"
 ];
 
 var catsText = {
     "Yes": "<strong>Yes</strong> (e.g., the response answers the question affirmatively)",
     "No": "<strong>No</strong> (e.g., the response answers the question negatively)",
-    "Not Sure": "<strong>Not Sure</strong> (e.g., you are unsure whether the response is yes or no)",
-    "Irrelevant": "<strong>Irrelevant</strong> (e.g. the response does not pertain to the question)"
+    "Not Sure": "<strong>Not Sure</strong> (e.g., you or the commenter are unsure about the response)",
+    "Irrelevant": "<strong>Irrelevant</strong> (e.g. the response does not pertain to the question)",
+    "NA": "<strong>N/A</strong> (e.g. the original poster is asking for or providing follow-up information)"
 };
 
 var examples = {
     "Yes": [
-        "<strong>Short Question:</strong> Oil Baths facilities in Qatar?",
-        "<strong>Full Question:</strong> Any suggestions for Spas having the facility of Oil Baths?",
-        "<strong>Response Subject:</strong> There is Turkish Hamams, I",
-        "<strong>Response Body:</strong> There is Turkish Hamams, I will have to check with my QL buddy Kellyheroes and get back to you!"
+        "<strong>Short Question:</strong> Salary Hold - is it legal?",
+        "<strong>Full Question:</strong> My friend is working as a sales executive.  The customer is not paying, so his company is holding his salary.  Is it legal for the company to do this?",
+        "<strong>Response Subject:</strong> RE: Salary Hold - is it legal?",
+        "<strong>Response Body:</strong> It is legal"
     ],
     "No": [
-        "<strong>Short Question:</strong> Oil Baths facilities in Qatar?",
-        "<strong>Full Question:</strong> Any suggestions for Spas having the facility of Oil Baths?",
-        "<strong>Response Subject:</strong> Brit",
-        "<strong>Response Body:</strong> It could be a good defence against intruders if any .."
+        "<strong>Short Question:</strong> Salary Hold - is it legal?",
+        "<strong>Full Question:</strong> My friend is working as a sales executive.  The customer is not paying, so his company is holding his salary.  Is it legal for the company to do this?",
+        "<strong>Response Subject:</strong> RE: Salary Hold - is it legal?",
+        "<strong>Response Body:</strong> It is illegal"
     ],
     "Not Sure": [
-        "<strong>Short Question:</strong> Oil Baths facilities in Qatar?",
-        "<strong>Full Question:</strong> Any suggestions for Spas having the facility of Oil Baths?",
-        "<strong>Response Subject:</strong> Please be careful. Remember",
-        "<strong>Response Body:</strong> Please be careful. Remember that crude oil can seep into the pores and can be toxic :O("
+        "<strong>Short Question:</strong> Work Visa",
+        "<strong>Full Question:</strong> I have been to Qatar for less than three months under a work visa, but was not issued residence. I then left. Can I come back under a work or visit visa?",
+        "<strong>Response Subject:</strong> RE: Work Visa",
+        "<strong>Response Body:</strong> You need to be a little more specific about your circumstances in order to get an answer to your question"
     ],
     "Irrelevant": [
-        "<strong>Short Question:</strong> Where can I get an ASUS laptop fixed?",
-        "<strong>Full Question:</strong> My ASUS laptop screen does not work, but everything else works still. Is there anywhere I can go to get it fixed?",
-        "<strong>Response Subject:</strong> RE: Where can I get an ASUS laptop fixed?",
-        "<strong>Response Body:</strong> do you have directions?"
+        "<strong>Short Question:</strong> Visit Syrian Refugee Camps?",
+        "<strong>Full Question:</strong> Is it possilbe for someone to visit Syrian Refugee camps?",
+        "<strong>Response Subject:</strong> Ahlan Ya",
+        "<strong>Response Body:</strong> Ahlan Ya Osama...anyone...which one is more sophisticated to make general interactions, interviews or volunteering... Radius..really?"
+    ],
+    "NA": [
+        "<strong>Short Question:</strong> Child Exit Permit",
+        "<strong>Full Question:</strong> My daughter will go to her aunt for vacation in the Philippines - does she need an exit permit or travel clearance?",
+        "<strong>Response Subject:</strong> RE: Child Exit Permit",
+        "<strong>Response Body:</strong> Thank you guys for the information - my daughter is less than 18 years old"
     ]
 };
 
